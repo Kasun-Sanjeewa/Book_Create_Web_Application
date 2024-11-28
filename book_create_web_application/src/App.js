4336f1423c9af80052ebb259fd87f4fb34caf339
Kasun3865 <ict21945@fot.sjp.ac.lk>
2024-11-29 02:14:44 +0530

    Book create page created

--- book_create_web_application/src/App.js
+++ book_create_web_application/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
+import BookCreate from './Components/BookCreate'
 
 export default function App() {
-    return (
-        <div>App</div>
-    )
+    const [books, setBook] = useState([])
+
+    const createBooks = (title) => {
+        const updateBooks = [
+            ...books,
+            {
+                id: Math.round(Math.random() * 9999),
+                title: title
+            }
+
+
+        ]
+        setBook(updateBooks)
+    }
+    return <div><BookCreate getTitleProp={createBooks} /></div>
 }
---
+++ book_create_web_application/src/Components/BookCreate.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function BookCreate({ getTitleProp }) {
+
+    const [title, setTitle] = useState('')
+
+    const handleChange = (event) => {
+        setTitle(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        getTitleProp(title)
+    }
+
+    return (
+        <div className='book-create'>
+            <form onSubmit={handleSubmit}>
+                <h3>Add a Book</h3>
+                <label>Title</label>
+                <input placeholder='Type here...' className='input' onChange={handleChange} value={title} />
+                <button className='button'>Create</button>
+            </form>
+        </div>
+    )
+}
